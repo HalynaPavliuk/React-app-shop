6f101c085d5a503eaa771ebb2991aa718ddb616e
HalynaPavliuk <84101675+Dilara31@users.noreply.github.com>
2021-12-06 20:00:48 +0200

    add gh-repo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
             <div className="nav-wrapper">
             <a href="#" className="brand-logo">React shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-             <li><a href="#">GitHub</a></li>
+             <li><a href="https://github.com/HalynaPavliuk/React-app-shop/">GitHub</a></li>
              
             </ul>
             </div>
